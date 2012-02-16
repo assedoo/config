e3b23c8688ef6f2d2db8746d0e125d5f3b340c5b
Pavel Zastavny <pzastavny@gmail.com>
2012-02-16 17:23:19 +0200

    Added main class Config and first test

---
+++ Config/src/main/java/org/dyndns/assedoo/config/Config.java
@@ -0,0 +1,118 @@
+/**
+ * @author assedoo
+ * @email assedoo@gmail.com
+ * @date Jan 27, 2012
+ *
+ */
+
+package org.dyndns.assedoo.config;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public final class Config {
+	
+	private static String configPropertiesFileName;
+	private Properties properties;
+	
+	private static Config _instance = null;
+	
+	public Config(String configPropertiesFileName) {
+		if (configPropertiesFileName == null) configPropertiesFileName = "config/config.properties";
+		Config.configPropertiesFileName = configPropertiesFileName;
+		FileInputStream in = null;
+		File configPropertiesFile = new File(configPropertiesFileName);
+		if (properties == null) {
+			properties = new Properties();
+		}
+		try {
+			in = new FileInputStream(configPropertiesFileName);
+		} catch (FileNotFoundException e) {
+			if (!configPropertiesFile.exists()) {
+				if (!createFile(configPropertiesFile)) {
+					if (createDir(configPropertiesFile)) {
+						if (createFile(configPropertiesFile)) {
+							try {
+								in = new FileInputStream(configPropertiesFileName);
+							} catch (FileNotFoundException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						}
+					}
+				}
+			}
+		}
+		try {
+			properties.load(in);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+	
+	public static synchronized Config getInstance() {
+		return getInstance(configPropertiesFileName);
+	}
+	
+	public static synchronized Config getInstance(String configPropertiesFileName) {
+        if (_instance == null)
+            _instance = new Config(configPropertiesFileName);
+        return _instance;
+    }
+	
+	/** Get property as an integer */
+	public String getString(String key){
+		return properties.getProperty(key);
+	}
+	
+	/** Set property as an integer */
+	public int getInt(String key){
+		return Integer.parseInt(getString(key));
+	}
+	
+	/** Get property */
+	public Properties getProperties() {
+		return properties;
+	}
+	
+	/** Set property */
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+		save();
+	}
+
+	private boolean createDir (File file) {
+		file.getParentFile().mkdir();
+		return true;
+	}
+	
+	private boolean createFile (File file) {
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	private void save() {
+		FileOutputStream out;
+		try {
+			out = new FileOutputStream(configPropertiesFileName);
+			properties.store(out, null);
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ Config/src/test/java/org/dyndns/assedoo/tests/MainTest.java
@@ -0,0 +1,18 @@
+/**
+ * @author pzastavny
+ * @email pzastavny@lohika.com
+ * @date Feb 16, 2012
+ *
+ */
+
+package org.dyndns.assedoo.tests;
+
+import org.dyndns.assedoo.config.Config;
+
+public class MainTest {
+	
+	public void perform() {
+		Config.getInstance();
+	}
+
+}
