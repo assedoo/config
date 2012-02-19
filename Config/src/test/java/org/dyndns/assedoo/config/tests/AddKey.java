fdc14c02c63750b9ba3803c48f05e98efb01c72e
Pavel Zastavny <pzastavny@gmail.com>
2012-02-19 16:56:28 +0200

    Chenged pom: added surefire plugin

--- Config/src/test/java/org/dyndns/assedoo/config/tests/AddKey.java
+++ Config/src/test/java/org/dyndns/assedoo/config/tests/AddKey.java
@@ -10,8 +10,6 @@ package org.dyndns.assedoo.config.tests;
 import java.io.File;
 import java.io.IOException;
 import org.dyndns.assedoo.config.Config;
-import org.junit.Before;
-import org.junit.Test;
 
 import junit.framework.TestCase;
 
@@ -20,13 +18,11 @@ public class AddKey extends TestCase {
 	private static final String KEY = "key";
 	private static final String FILENAME = "config/config.properties";
 	
-	@Before
 	public void setUp() throws IOException {
 	
 		new File(FILENAME).delete();
 	}
 	
-	@Test
 	public void testAddKey() throws IOException {
 		Config.getInstance().addKey(KEY);
 		
