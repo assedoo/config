648b27b8b4a5b2fcd830c1b87be3616d5753f6cd
Pavel Zastavny <pzastavny@gmail.com>
2012-02-21 11:52:22 +0200

    Added putValue

--- Config/src/main/java/org/dyndns/assedoo/config/Config.java
+++ Config/src/main/java/org/dyndns/assedoo/config/Config.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public final class Config {
-	
+
 	private static volatile Config instance;
 	private static String configFileName;
 	private File configFile;
@@ -24,66 +24,81 @@ public final class Config {
 
 	private Config() throws IOException {
 		configFile = new File(configFileName);
-		if (!configFile.exists()) createFile(configFile);
+		if (!configFile.exists())
+			createFile(configFile);
 		fileInputStream = new FileInputStream(configFile);
 		properties = new Properties();
 		properties.load(fileInputStream);
 		fileInputStream.close();
 	}
-	
-	/** Get Config instance.
-	 * 	Properties will be read from file: config/config.properties.
+
+	/**
+	 * Get Config instance. Properties will be read from file:
+	 * config/config.properties.
 	 */
 	public static Config getInstance() throws IOException {
 		Config.configFileName = "config/config.properties";
 		return initialization();
 	}
-	/** Get Config instance.
-	 * 	Properties will be read from file: configFileName.
+
+	/**
+	 * Get Config instance. Properties will be read from file: configFileName.
 	 */
 	public static Config getInstance(String configFileName) throws IOException {
-		if (configFileName == null) Config.configFileName = "config/config.properties";
-		else Config.configFileName = configFileName;
+		if (configFileName == null)
+			Config.configFileName = "config/config.properties";
+		else
+			Config.configFileName = configFileName;
 		return initialization();
 	}
-	
+
 	/** Reload properties from file */
 	public void reload() throws IOException {
 		fileInputStream = new FileInputStream(configFile);
 		properties.load(fileInputStream);
 	}
-	
+
 	/** Get property as an string */
 	public String getString(String key) {
 		return properties.getProperty(key);
 	}
-	
+
 	/** Get property as an int */
 	public int getInt(String key) {
 		return Integer.parseInt(getString(key));
 	}
-	
-	/** Add key to Properties
-	 *  Note: Use save() method to save changes.
+
+	/**
+	 * Add key to Properties Note: Use save() method to save changes.
 	 */
 	public void addKey(String key) {
 		properties.put(key, "");
 	}
-	
-	/** Add key to Properties with string value
-	 *  Note: Use save() method to save changes.
+
+	/**
+	 * Add key to Properties with string value Note: Use save() method to save
+	 * changes.
 	 */
 	public void addKey(String key, String value) {
 		properties.put(key, value);
 	}
-	
-	/** Add key to Properties with int value
-	 *  Note: Use save() method to save changes.
+
+	/**
+	 * Add key to Properties with int value Note: Use save() method to save
+	 * changes.
 	 */
 	public void addKey(String key, int value) {
 		properties.put(key, value);
 	}
-	
+
+	/**
+	 * Add key to Properties with Object value Note: Use save() method to save
+	 * changes.
+	 */
+	public void addKey(Object key, Object value) {
+		properties.put(key, value);
+	}
+
 	/** Save changes */
 	public void save() throws IOException {
 		FileOutputStream fileOutputStream = null;
@@ -95,31 +110,37 @@ public final class Config {
 		properties.store(fileOutputStream, null);
 		fileOutputStream.close();
 	}
-	
+
 	/** Get Properties */
 	public Properties getProperties() {
 		return properties;
 	}
-	
+
+	/** Maps the specified key to the specified value */
+	public void putValue(String key, Object value) {
+		properties.put(key, value);
+	}
+
 	private void createFile(File configFile) throws IOException {
-		if (!configFile.getParentFile().exists()) createDirectories(configFile);
+		if (!configFile.getParentFile().exists())
+			createDirectories(configFile);
 		configFile.createNewFile();
 	}
-	
+
 	private void createDirectories(File configFile) {
 		configFile.getParentFile().mkdirs();
 	}
-	
+
 	private static Config initialization() throws IOException {
 		Config localInstance = instance;
-    	if (localInstance == null) {
-    	synchronized (Config.class) {
-                localInstance = instance;
-                if (localInstance == null) {
-                    instance = localInstance = new Config();
-                }
-            }
-        }
-        return localInstance;
-    }
+		if (localInstance == null) {
+			synchronized (Config.class) {
+				localInstance = instance;
+				if (localInstance == null) {
+					instance = localInstance = new Config();
+				}
+			}
+		}
+		return localInstance;
+	}
 }
---
+++ Config/src/test/java/org/dyndns/assedoo/config/tests/PutValueTest.java
@@ -0,0 +1,38 @@
+/**
+ * @author pzastavny
+ * @email pzastavny@lohika.com
+ * @date Feb 21, 2012
+ *
+ */
+
+package org.dyndns.assedoo.config.tests;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.dyndns.assedoo.config.Config;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PutValueTest extends TestCase {
+	
+	private static final String KEY = "key";
+	private static final String FILENAME = "config/config.properties";
+	private static final Object VALUE = 1;
+	
+	@Before
+	public void setUp() throws IOException {
+	
+		new File(FILENAME).delete();
+	}
+	
+	@Test
+	public void testPutValueObject() throws IOException {
+		Config.getInstance().putValue(KEY, VALUE);
+
+		assertTrue(Config.getInstance().getProperties().containsKey(KEY));	
+	}
+
+}
