6f996e5fa552e4a5b9cc84e331d4d67d36eab174
Pavel Zastavnyi <assedoo@gmail.com>
2012-08-27 11:18:52 +0300

    Changed AddKeyObjectTest

--- Config/src/test/java/org/dyndns/assedoo/config/tests/AddKeyIntTest.java
+++ Config/src/test/java/org/dyndns/assedoo/config/tests/AddKeyIntTest.java
@@ -1,38 +1,57 @@
 /**
- * @author assedoo
- * @email assedoo@gmail.com
- * @date Feb 19, 2012
- *
+ * 
  */
-
 package org.dyndns.assedoo.config.tests;
 
-import java.io.File;
 import java.io.IOException;
-
-import org.dyndns.assedoo.config.Config;
-import org.junit.Before;
-import org.junit.Test;
 
 import junit.framework.TestCase;
 
+import org.dyndns.assedoo.config.Config;
+import org.dyndns.assedoo.config.utils.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author assedoo
+ * @email assedoo@gmail.com
+ * @date Aug 26, 2012
+ * @time 7:48:05 PM
+ *
+ */
 public class AddKeyIntTest extends TestCase {
 	
 	private static final String KEY = "key";
 	private static final String FILENAME = "config/config.properties";
 	private static final int VALUE = 1;
-	
-	@Before
-	public void setUp() throws IOException {
-	
-		new File(FILENAME).delete();
-	}
-	
-	@Test
-	public void testAddKeyInt() throws IOException {
-		Config.getInstance().addKey(KEY, VALUE);
 
-		assertTrue(Config.getInstance().getProperties().containsKey(KEY));	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		FileUtils.deleteFile(FILENAME);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		FileUtils.deleteFile(FILENAME);
+	}
+
+	@Test
+	public void testAddKeyInt() {
+		boolean result = false;
+		try {
+			Config.getInstance().addKey(KEY, VALUE);
+			result = Config.getInstance().getProperties().containsKey(KEY);
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}	
+		assertTrue(result);	
 	}
 
 }
--- Config/src/test/java/org/dyndns/assedoo/config/tests/AddKeyObjectTest.java
+++ Config/src/test/java/org/dyndns/assedoo/config/tests/AddKeyObjectTest.java
@@ -1,10 +1,6 @@
 /**
- * @author assedoo
- * @email assedoo@gmail.com
- * @date Feb 21, 2012
- *
+ * 
  */
-
 package org.dyndns.assedoo.config.tests;
 
 import java.io.File;
@@ -13,26 +9,49 @@ import java.io.IOException;
 import junit.framework.TestCase;
 
 import org.dyndns.assedoo.config.Config;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author assedoo
+ * @email assedoo@gmail.com
+ * @date Aug 27, 2012
+ * @time 11:07:06 AM
+ *
+ */
 public class AddKeyObjectTest extends TestCase {
 	
 	private static final String KEY = "key";
 	private static final String FILENAME = "config/config.properties";
 	private static final Object VALUE = 1;
-	
+
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@Before
-	public void setUp() throws IOException {
-	
+	public void setUp() throws Exception {
 		new File(FILENAME).delete();
 	}
-	
-	@Test
-	public void testAddKeyObject() throws IOException {
-		Config.getInstance().addKey(KEY, VALUE);
 
-		assertTrue(Config.getInstance().getProperties().containsKey(KEY));	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		new File(FILENAME).delete();
+	}
+
+	@Test
+	public void testAddKeyObject() {
+		boolean result = false;
+		try {
+			Config.getInstance().addKey(KEY, VALUE);
+			result = Config.getInstance().getProperties().containsKey(KEY);
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+		assertTrue(result);
 	}
 
 }
